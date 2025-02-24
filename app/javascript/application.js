93b4dbc98bc28d32a8bae3191b10447807f91ab2
jiherrera <jherrera@ecaresoft.com>
2025-02-24 00:14:20 -0600

    Initializing project

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Entry point for the build script in your package.json.
+import "@hotwired/turbo-rails"
+
+// Stimulus controllers.
+import "./controllers"
+
+import 'jquery';
+
+import Rails from 'rails-ujs';
